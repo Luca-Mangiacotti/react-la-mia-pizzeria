0b34f0c95ba105aa1ad4da26d0dc76a586ccde37
Luca-Mangiacotti <lucamangiacotti100@gmail.com>
2025-07-02 18:57:32 +0200

    feat add axios and main route

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,1 +1,32 @@
-export default function HomePage() {}
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+export default function HomePage() {
+  const [pizzas, setPizzas] = useState([]);
+
+  //esegiamo il rendering delle pizze
+  const fetchPizzas = () => {
+    console.log("caricando le pizze");
+    axios
+      .get(import.meta.env.VITE_API_URL)
+      .then((response) => {
+        console.log(response.data);
+        const { data } = response;
+        setPizzas(data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+  useEffect(fetchPizzas, []);
+  return (
+    <>
+      <h1 className="text-primary">Pizzeria 8 Bit</h1>
+      <section>
+        {pizzas.map((pizza) => (
+          <PizzaCard key={pizza.id} pizza={pizza} />
+        ))}
+      </section>
+    </>
+  );
+}
