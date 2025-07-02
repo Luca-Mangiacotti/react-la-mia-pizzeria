a0200d1bd3436062a4a2f6773e560164bf342562
Luca-Mangiacotti <lucamangiacotti100@gmail.com>
2025-07-02 18:28:07 +0200

    fix import of reactrouter feat add initial structure of pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
-import { BrowserRouter, Routes, Route } from "react-router";
+import "@popperjs/core/dist/umd/popper.min.js";
+import "bootstrap/dist/js/bootstrap.min.js";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import DefaultLayout from "./layout/DefaultLayout";
+import HomePage from "./pages/HomePage";
+import PizzaDetail from "./pages/PizzaDetail";
 
 export default function App() {
   return (
@@ -8,7 +12,8 @@ export default function App() {
       <BrowserRouter>
         <Routes>
           <Route element={<DefaultLayout />}>
-            {/*   qui le nostre rotte */}
+            <Route index path="/" element={<HomePage />}></Route>
+            <Route path="/pizzas/:id" element={<PizzaDetail />}></Route>
           </Route>
         </Routes>
       </BrowserRouter>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,13 @@
+import { Link } from "react-router-dom";
+
 export default function Header() {
-  return <header></header>;
+  return (
+    <header>
+      <nav className="navbar mb4">
+        <div className="container-fluid justify-content-start">
+          <Link to="/">Homepage</Link>
+        </div>
+      </nav>
+    </header>
+  );
 }
